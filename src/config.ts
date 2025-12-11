0157612a1ada7a40d202f1c525f1f3a0e84db6c0
tulio1004 <tulio1004@gmail.com>
2025-12-11 08:14:58 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,15 @@
 import { ServerOptions } from './types/ServerOptions';
 
-const config: ServerOptions = {
-  secretKey: process.env.SECRET_KEY || 'THISISMYSECURETOKEN',
-  host: process.env.HOST || 'http://localhost',
-  port: process.env.PORT || '21465',
-
-  deviceName: process.env.DEVICE_NAME || 'WppConnectServer',
+export default {
+  secretKey: 'THISISMYSECURETOKEN',
+  host: 'http://localhost',
+  port: '21465',
+  deviceName: 'WppConnect',
   poweredBy: 'WPPConnect-Server',
-
-  startAllSession: false,
+  startAllSession: true,
   tokenStoreType: 'file',
   maxListeners: 15,
-
-  customUserDataDir:
-    process.env.USER_DATA_DIR || '/usr/src/wpp-server/userDataDir/',
-
+  customUserDataDir: './userDataDir/',
   webhook: {
     url: null,
     autoDownload: true,
@@ -31,57 +26,89 @@ const config: ServerOptions = {
     onSelfMessage: false,
     ignore: ['status@broadcast'],
   },
-
   websocket: {
     autoDownload: false,
     uploadS3: false,
   },
-
   chatwoot: {
     sendQrCode: true,
     sendStatus: true,
   },
-
   archive: {
     enable: false,
     waitTime: 10,
     daysToArchive: 45,
   },
-
   log: {
-    level: 'silly',
+    level: 'silly', // Before open a issue, change level to silly and retry a action
     logger: ['console', 'file'],
   },
-
-  // ---------------------------------------------------
-  // 🔥 AQUI ESTÁ A CORREÇÃO CRÍTICA DO BROWSER
-  // ---------------------------------------------------
- createOptions: {
-  puppeteerOptions: {
-    headless: true,
-    executablePath: '/usr/bin/google-chrome-stable',
-    args: [
+  createOptions: {
+    browserArgs: [
+      '--disable-web-security',
       '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-gpu',
-      '--disable-extensions',
-      '--disable-features=IsolateOrigins,site-per-process',
+      '--disable-web-security',
+      '--aggressive-cache-discard',
+      '--disable-cache',
+      '--disable-application-cache',
+      '--disable-offline-load-stale-cache',
+      '--disk-cache-size=0',
       '--disable-background-networking',
       '--disable-default-apps',
+      '--disable-extensions',
       '--disable-sync',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
       '--disable-translate',
       '--hide-scrollbars',
       '--metrics-recording-only',
       '--mute-audio',
       '--no-first-run',
+      '--safebrowsing-disable-auto-update',
       '--ignore-certificate-errors',
       '--ignore-ssl-errors',
+      '--ignore-certificate-errors-spki-list',
     ],
+    /**
+     * Example of configuring the linkPreview generator
+     * If you set this to 'null', it will use global servers; however, you have the option to define your own server
+     * Clone the repository https://github.com/wppconnect-team/wa-js-api-server and host it on your server with ssl
+     *
+     * Configure the attribute as follows:
+     * linkPreviewApiServers: [ 'https://www.yourserver.com/wa-js-api-server' ]
+     */
+    linkPreviewApiServers: null,
+
+    /**
+     * Set specific whatsapp version
+     */
+    // whatsappVersion: '2.xxxxx',
   },
-},
-
-  // ---------------------------------------------------
-};
-
-export default config;
+  mapper: {
+    enable: false,
+    prefix: 'tagone-',
+  },
+  db: {
+    mongodbDatabase: 'tokens',
+    mongodbCollection: '',
+    mongodbUser: '',
+    mongodbPassword: '',
+    mongodbHost: '',
+    mongoIsRemote: true,
+    mongoURLRemote: '',
+    mongodbPort: 27017,
+    redisHost: 'localhost',
+    redisPort: 6379,
+    redisPassword: '',
+    redisDb: 0,
+    redisPrefix: 'docker',
+  },
+  aws_s3: {
+    region: 'sa-east-1' as any,
+    access_key_id: null,
+    secret_key: null,
+    defaultBucketName: null,
+    endpoint: null,
+    forcePathStyle: null,
+  },
+} as unknown as ServerOptions;
