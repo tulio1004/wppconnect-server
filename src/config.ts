7c281a09b1ffb210fe7e2727f66b00a8324a46cd
tulio1004 <tulio1004@gmail.com>
2025-12-10 22:13:08 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export default {
   port: '21465',
   deviceName: 'WppConnect',
   poweredBy: 'WPPConnect-Server',
-  startAllSession: true,
+  startAllSession: false,
   tokenStoreType: 'file',
   maxListeners: 15,
   customUserDataDir: './userDataDir/',
